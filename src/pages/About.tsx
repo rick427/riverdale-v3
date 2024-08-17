a6078bcb56e5012789aa9e742a33a0f6ce82a100
Richard <richardnjoku427@gmail.com>
2024-08-17 19:26:31 +0300

    Latest changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,9 +3,9 @@ import Layout from "@/components/base/Layout";
 //@: Components
 import Hero from "@/components/ui/About/Hero";
 import Founders from "@/components/ui/About/Founders";
-import Company from "@/components/ui/About/Company";
-import Teams from "@/components/ui/About/Team";
-import Banner from "@/components/ui/About/Banner";
+// import Company from "@/components/ui/About/Company";
+// import Teams from "@/components/ui/About/Team";
+// import Banner from "@/components/ui/About/Banner";
 
 export default function About() {
     return (
